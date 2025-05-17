9d9bc41d733cdeaa918ac9a85f058a3b0740bf54
PanmeansBread <haitranduc60@gmail.com>
2025-05-18 00:06:19 +0700

    re added proper files

---
+++ demo/src/main/java/dev/SpringTutorial/spring/SPproject/SpringTimeApplication.java
@@ -0,0 +1,13 @@
+package dev.SpringTutorial.spring.SPproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTimeApplication.class, args);
+	}
+
+}
